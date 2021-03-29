2257d2a73535775f4a46a52e65d0061f0142aca3
Lakshit Dabas <lakshitdabas@hotmail.com>
2021-03-29 18:08:24 +0100

    Setting up Pennylane with BraKet

---
+++ main.py
@@ -0,0 +1,15 @@
+import pennylane as qml
+from pennylane import numpy as np
+
+s3 = ("my-bucket", "my-prefix")
+remote_device = qml.device("braket.aws.qubit", device_arn="arn:aws:braket:::device/quantum-simulator/amazon/sv1", s3_destination_folder=s3, wires=2)
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
